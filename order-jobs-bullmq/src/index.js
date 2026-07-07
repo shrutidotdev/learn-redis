5236c31bd3bb1c92feb3e7b995a76f635c40de90
shruti singh <shrutisinghskr@gmail.com>
2026-07-07 17:17:49 +0530

    chore: add order-jobs-bullmq package with BullMQ, Express, ioredis, and Docker setup

---
+++ order-jobs-bullmq/src/index.js
@@ -0,0 +1,13 @@
+import express from "express"
+import Redis from "ioredis"
+
+const app = express()
+app.use(express.json())
+
+const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379')
+
+
+
+app.listen(3000, () => {
+    console.log('Server running.......')
+})
